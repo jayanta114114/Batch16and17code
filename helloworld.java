846e2327c8dacb4b0ccf031ae64f236f55d8622e
jayanta114114 <84843456+jayanta114114@users.noreply.github.com>
2021-06-13 11:26:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch21 team");
-      System.out.println("Hello batch16 and batch17 students and training practicing ");
+     System.out.println("Hello poll scm trigger jenkins job automatically ");
     }  
 }  
